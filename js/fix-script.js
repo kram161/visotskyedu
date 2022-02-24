2176b2c507e7c30285b040eb363b84614f575dbb
kram161 <39655243+kram161@users.noreply.github.com>
2022-02-24 15:40:49 +0300

    upd

--- js/fix-script.js
+++ js/fix-script.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const moreDescription = document.querySelectorAll('.programm-block__unit__more-information__description');
     const moreCloseButton = document.querySelectorAll('.more-information__close-button')
     for(let i = 0;moreButton.length; i++){
-        moreDescription[i].style.opacity = '-1';
+        moreDescription[i].style.display = 'none';
         moreDescription[i].style.zIndex = '-1';
         moreCloseButton[i].style.opacity = '1';
         moreButton[i].addEventListener('click', function (){
-            if(moreDescription[i].style.opacity === '-1'){
-                moreDescription[i].style.opacity = '1';
+            if(moreDescription[i].style.display === 'none'){
+                moreDescription[i].style.display = 'block';
                 moreDescription[i].style.zIndex = '1';
                 moreCloseButton[i].style.opacity = '0'
             }
             else{
-                moreDescription[i].style.opacity = '-1';
+                moreDescription[i].style.display = 'none';
                 moreDescription[i].style.zIndex = '-1';
                 moreCloseButton[i].style.opacity = '1'
             }
