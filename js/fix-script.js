ea2ff35c465fe8d7887643254da70b9291371ee6
kram161 <39655243+kram161@users.noreply.github.com>
2022-03-03 15:32:11 +0300

    Update fix-script.js

--- js/fix-script.js
+++ js/fix-script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const moreButton = document.querySelectorAll('.programm-block__unit__more-information__button');
     const moreDescription = document.querySelectorAll('.programm-block__unit__more-information__description');
     const moreCloseButton = document.querySelectorAll('.more-information__close-button')
-    for(let i = 0;moreButton.length; i++){
+    for(let i = 0;i < moreButton.length; i++){
         moreDescription[i].style.display = 'none';
         moreDescription[i].style.zIndex = '-1';
         moreCloseButton[i].style.opacity = '1';
